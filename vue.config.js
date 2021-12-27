d637fba12295ef016ad8ebfe7cc47a32f5813a6b
Muzafar Umarov <umarov@users.noreply.github.com>
2021-12-26 21:59:34 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,6 @@ module.exports = {
   },
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "https://umarov.dev/code-shop-timer/"
+      ? "https://umarov.dev/vue-timer/"
       : "/"
 };
